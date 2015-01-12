7ac4b97e5650a32d4cbb3ec1565adbbbbe016eee
dannysoul <dannysoulchild@gmail.com>
2015-01-12 12:32:40 +0100

    Test class updated

--- TravelQuiz/TravelQuiz/Models/Test.cs
+++ TravelQuiz/TravelQuiz/Models/Test.cs
@@ -7,5 +7,10 @@ namespace TravelQuiz.Models
 {
     public class Test
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int Public { get; set; }
+        public virtual ICollection<Question> Question { get; set; }
     }
 }
