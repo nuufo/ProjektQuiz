f28ea0df93611556dccf3fbfb9191f01fb020aae
dannysoul <dannysoulchild@gmail.com>
2015-01-12 18:57:30 +0100

    lalla

--- TravelQuiz/TravelQuiz/Repository/TestContext.cs
+++ TravelQuiz/TravelQuiz/Repository/TestContext.cs
@@ -16,7 +16,5 @@ namespace TravelQuiz.Repository
         public DbSet<Test> Tests { get; set; }
         public DbSet<Question> Questions { get; set; }
         public DbSet<Option> Options { get; set; }
-
-
     }
 }
